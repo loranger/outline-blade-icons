4275efa3bfa9d925690ae7ae34c1aeef2b0a4a66
Laurent Goussard <loranger@free.fr>
2024-09-29 19:02:04 +0200

    fix: Fix component path

--- src/OutlineIconsServiceProvider.php
+++ src/OutlineIconsServiceProvider.php
@@ -16,7 +16,7 @@ class OutlineIconsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../resources/views/components/outline' => resource_path('views/components/icons'),
+            __DIR__ . '/../resources/views/components/outline' => resource_path('views/components/outline'),
         ], 'outline');
     }
 }
